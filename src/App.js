8fc8df7c78e567a3e0805694ed09ad7bf157c8cc
Williamrobert Green <wrg2996@c02xj29rjg5mmbp.attlocal.net>
2021-01-12 16:25:43 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Back, HeaderBar, Spacer } from './components/styled';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Back>
+      <HeaderBar>
+        <h2>Movie Ratings</h2>
+        <Spacer />
+        <h4>Login</h4>
+      </HeaderBar>
+      <h3>Hello War of the World!</h3>
+    </Back>
   );
 }
 
 export default App;
+
