524432981d80644e3c7c109a2ad1afc96628cd42
kjintroverted <wkgreen13@gmail.com>
2022-05-31 14:30:41 -0400

    allows click on card to view

--- src/components/recipes/RecipePreview.js
+++ src/components/recipes/RecipePreview.js
@@ -12,30 +12,29 @@ function RecipePreview({ recipe, onDragEnd, onDrag = () => { } }) {
   }
 
   return (
-    <Card className="click"
-      draggable="true"
-      onDragStart={ handleDrag }
-      onDragEnd={ onDragEnd }
-    >
-      <Title>{ recipe.name }</Title>
-      <Row>
-        {
-          recipe.tags &&
-          recipe.tags.map(t => <Chip key={ t } label={ t } size="small" />)
-        }
-      </Row>
-      <Spacer height=".5em" />
-      <Divider theme={ THEME } />
-      <Label>Needs</Label>
-      <CardContent>
-        { recipe.ingredients.map(i => i.item).join(', ') }
-      </CardContent>
-      <ActionBar>
-        <Link to={ `/recipes/${ resourceName(recipe.thing.url) }` }>
-          <Button variant="text" color="primary">View</Button>
-        </Link>
-      </ActionBar>
-    </Card >
+    <Link
+      style={ { width: "95%" } }
+      to={ `/recipes/${ resourceName(recipe.thing.url) }` }>
+      <Card className="click"
+        draggable="true"
+        onDragStart={ handleDrag }
+        onDragEnd={ onDragEnd }
+      >
+        <Title>{ recipe.name }</Title>
+        <Row>
+          {
+            recipe.tags &&
+            recipe.tags.map(t => <Chip key={ t } label={ t } size="small" />)
+          }
+        </Row>
+        <Spacer height=".5em" />
+        <Divider theme={ THEME } />
+        <Label>Needs</Label>
+        <CardContent>
+          { recipe.ingredients.map(i => i.item).join(', ') }
+        </CardContent>
+      </Card >
+    </Link>
   )
 }
 
